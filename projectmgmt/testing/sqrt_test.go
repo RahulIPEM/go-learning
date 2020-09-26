de3ab1a5bec10cf0dc3b375a7af967797d9f2c9e
Rahul Sharma <rs5802@gmail.com>
2020-09-26 23:37:05 +0530

    done: reading from csv file for sqrt

--- projectmgmt/testing/sqrt_test.go
+++ projectmgmt/testing/sqrt_test.go
@@ -1,19 +1,55 @@
 package sqrt
 
-import "testing"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"testing"
+)
 
 func almostEqual(v1, v2 float64) bool {
 	return Abs(v1-v2) <= 0.001
 }
 
 func TestSimple(t *testing.T) {
-	val, err := Sqrt(2)
-
+	file, err := os.Open("sqrt_cases.csv")
 	if err != nil {
-		t.Fatalf("error in calculation = %s", err)
+		t.Fatalf("can't open cases file - %s", err)
 	}
+	defer file.Close()
 
-	if !almostEqual(val, 1.414214) {
-		t.Fatalf("bad value - %f", val)
+	rdr := csv.NewReader(file)
+	for {
+		record, err := rdr.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			t.Fatalf("error reading cases file - %s", err)
+		}
+
+		val, err := strconv.ParseFloat(record[0], 64)
+		if err != nil {
+			t.Fatalf("Bad value - %s", record[0])
+		}
+
+		expected, err := strconv.ParseFloat(record[1], 64)
+		if err != nil {
+			t.Fatalf("Bad value - %s", record[1])
+		}
+
+		t.Run(fmt.Sprintf("%f", val), func(t *testing.T) {
+			out, err := Sqrt(val)
+			if err != nil {
+				t.Fatal("error")
+			}
+
+			if !almostEqual(out, expected) {
+				t.Fatalf("%f != %f", out, expected)
+			}
+		})
 	}
 }
